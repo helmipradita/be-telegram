33d4a2a41c9f89b3cb09090a3966bab61e127a0b
Helmi Pradita <helmipraditaa@gmail.com>
2023-01-19 02:43:18 +0700

    update endpoint chats

--- src/models/chats.js
+++ src/models/chats.js
@@ -5,8 +5,8 @@ const store = (data) => {
   console.log(data);
   return new Promise((resolve, reject) => {
     Pool.query(
-      `INSERT INTO chats (sender_id, receiver_id, message) 
-        VALUES ('${sender}', '${receiver}', '${message}')`,
+      `INSERT INTO chats (sender_id, receiver_id, message, created_at, updated_at) 
+        VALUES ('${sender}', '${receiver}', '${message}', NOW(), NOW())`,
       (err, res) => {
         if (err) {
           reject(err);
@@ -21,7 +21,10 @@ const list = (sender, receiver) => {
   return new Promise((resolve, reject) => {
     console.log(sender, receiver, 'test');
     Pool.query(
-      `SELECT chats.id, chats.message, userSender.username AS sender, userReceiver.username AS receiver
+      `SELECT chats.id, chats.message, 
+          userSender.username AS sender, 
+          userReceiver.username AS receiver,
+          to_char( chats.created_at, 'HH:MI DD/Mon/YYYY' ) AS created_at,
         FROM chats as chats
         LEFT JOIN users AS userSender ON chats.sender_id=userSender.id
         LEFT JOIN users AS userReceiver ON chats.receiver_id=userReceiver.id
